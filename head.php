b56b30947261b1f49f7646e2c2fbd92eb381e00c
Alex Vong <avong003@gmail.com>
2021-06-02 13:44:42 -0700

    create head php

---
+++ head.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+		<meta charset="UTF-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<meta
+			name="description"
+			content="[[meta-description]]"
+		/>
+		<meta
+			name="og:description"
+			content="[[meta-description]]"
+		/>
+		<meta property="og:image" content="[[meta-image]]" />
+		<meta property="og:title" content="[[meta-content]]" />
+		<link rel="shortcut icon" type="image/jpg" href="[[favicon]]" />
+
+		<link rel="stylesheet" href="css/styles.css" />
+		<title>Responsive Layout Challenge</title>
+</head>
