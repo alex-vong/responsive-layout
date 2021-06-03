4ee567daef31baf4b5a9399bdbaf7c778684005f
Alex Vong <avong003@gmail.com>
2021-06-02 18:00:52 -0700

    added head php

--- head.php
+++ head.php
@@ -5,15 +5,15 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 		<meta
 			name="description"
-			content="[[meta-description]]"
+			content="Ivy’s Super Ultra Responsive Layout Challenge!"
 		/>
 		<meta
 			name="og:description"
-			content="[[meta-description]]"
+			content="Ivy’s Super Ultra Responsive Layout Challenge!"
 		/>
-		<meta property="og:image" content="[[meta-image]]" />
-		<meta property="og:title" content="[[meta-content]]" />
-		<link rel="shortcut icon" type="image/jpg" href="[[favicon]]" />
+		<meta property="og:image" content="https://peprojects.dev/alpha-1/alexvong/responsive-layout/images/responsive-meta.png" />
+		<meta property="og:title" content="Alex codes some stuff" />
+		<link rel="shortcut icon" type="image/jpg" href="images/alex.png" />
 
 		<link rel="stylesheet" href="css/styles.css" />
 		<title>Responsive Layout Challenge</title>
