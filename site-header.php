382d5d1540f940e3198121cad38bc1aa5d2bfd07
Alex Vong <avong003@gmail.com>
2021-06-02 18:01:03 -0700

    commit

---
+++ site-header.php
@@ -0,0 +1,15 @@
+<header class="site-header">
+	<div class="inner-column">
+		<nav-bar>
+			<nav class="nav-menu">
+				<a href="#">Logo</a>
+				<a href="#">Some page</a>
+				<a href="#">"Masthead"</a>
+			</nav>
+
+			<nav class="sign-in">
+				<a href="#">Sign-In</a>
+			</nav>
+		</nav-bar>
+	</div>
+</header>
