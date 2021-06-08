600cd7c42b149e1ea2cf55fe17cb29216bcdbfb8
Alex Vong <avong003@gmail.com>
2021-06-08 14:05:20 -0700

    moved cta

--- index.php
+++ index.php
@@ -1,44 +1,21 @@
 <?php include('head.php')?>
 
-<body>
+	<body>
 
-	<?php include('site-header.php')?>
-	<?php include('hero-section.php')?>
+		<?php include('site-header.php')?>
+		<?php include('hero-section.php')?>
+		
+		<section class="call-to-action">
+			<?php include('cta.php')?>
+		</section>
 
-	<!-- <?php include('site-header.php')?> -->
-	<!-- <?php include('site-header.php')?> -->
+		<?php include('article-grid.php')?>
 
+		<section class="call-to-action cta-red">
+			<?php include('cta.php')?>
+		</section>
 
-
-
-	
-	<section class="call-to-action">
-		<div class="inner-column">
-			<section class="cta-module">
-				<h2>This module is a &#34;call to action&#34;</h2>
-				<p class="supporting-text">
-					These aren't names you should memorize. We're just making them up. Sometimes things get called something so many times, that it sticks. This is a pretty common pattern.
-				</p>
-				<button>Here&#39;s the action!</button>
-			</section>
-		</div>
-	</section>
-
-	<?php include('article-grid.php')?>
-
-	<section class="call-to-action cta-red">
-		<div class="inner-column">
-			<section class="cta-module">
-				<h2>This module is a &#34;call to action&#34;</h2>
-				<p class="supporting-text">
-					These aren't names you should memorize. We're just making them up. Sometimes things get called something so many times, that it sticks. This is a pretty common pattern.
-				</p>
-				<button>Here&#39;s the action!</button>
-			</section>
-		</div>
-	</section>
-
-<?php include('footer.php')?>
-	
-</body>
+		<?php include('footer.php')?>
+		
+	</body>
 </html>
