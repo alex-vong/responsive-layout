c2046a076ae29c5221d76c9a5b566fc8c3012f55
Alex Vong <avong003@gmail.com>
2021-06-08 14:04:41 -0700

    added cta php

---
+++ cta.php
@@ -0,0 +1,18 @@
+<div class="inner-column">
+	<section class="cta-module">
+		<?php 
+		$cta = [
+			"heading"=> "This module is a &#34;call to action&#34;.",
+			"text" => "These aren't names you should memorize. We're just making them up. Sometimes things get called something so many times, that it sticks. This is a pretty common pattern.",
+			"button" => "Here&#39;s the action!",
+		];
+		
+		?>
+
+		<h2> <?=$cta['heading']?> </h2>
+		<p class="supporting-text">
+			<?=$cta['text']?>
+		</p>
+		<a href="#" class="button"> <?=$cta['button']?> </a>
+	</section>
+</div>
